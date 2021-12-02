81045e7c878c5a43dead216a8c951cf47012b6ad
EqualMa <equalma@outlook.com>
2021-12-02 15:50:02 +0000

    feat: web page

---
+++ src/data.ts
@@ -0,0 +1,33 @@
+import fullData from "../public/data.json";
+
+function getCategories(data: typeof fullData) {
+  let _categories = data.emoticon_format.map((cate) => ({
+    ...cate,
+    value: cate.value.map((emo) => {
+      const ext = emo.url.slice(emo.url.lastIndexOf("."));
+      const url = `/emoticon/${emo.value}${ext}`;
+      return {
+        ...emo,
+        originalUrl: emo.url,
+        url,
+      };
+    }),
+  }));
+
+  let _emoticonsUrlToData = new Map(
+    _categories.flatMap((c) => c.value.map((emo) => [emo.originalUrl, emo]))
+  );
+
+  const categories = _categories.map((c) => ({
+    ...c,
+    url: _emoticonsUrlToData.get(c.url)?.url,
+  }));
+
+  return categories;
+}
+
+export const categories = getCategories(fullData);
+
+export type EmoticonCategoryData = typeof categories[number];
+
+export type Emoticon = EmoticonCategoryData["value"][number];
